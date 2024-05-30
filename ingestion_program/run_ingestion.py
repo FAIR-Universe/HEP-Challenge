aa3804073c1a439b8f142358f7a54f4f35a41d54
Chris Harris <cjh@lbl.gov>
2024-05-30 08:59:17 -0700

    Guard with __main__ check
    Otherwise this will be executed on import, for example, when
    multiprocessing is being used.

--- ingestion_program/run_ingestion.py
+++ ingestion_program/run_ingestion.py
@@ -92,89 +92,91 @@ parser.add_argument(
     help="Whether to run ingestion in parallel",
 )
 
-args = parser.parse_args()
+if __name__ == "__main__":
 
-if args.parallel:
-    from ingestion_parallel import Ingestion
-else:
-    from ingestion import Ingestion
+    args = parser.parse_args()
 
-
-if not args.codabench:
-    input_dir = args.input
-    output_dir = args.output
-    submission_dir = args.submission
-    program_dir = os.path.join(root_dir_name, "ingestion_program")
-else:
-    input_dir = "/app/input_data"
-    output_dir = "/app/output"
-    submission_dir = "/app/ingested_program"
-    program_dir = "/app/program"
+    if args.parallel:
+        from ingestion_parallel import Ingestion
+    else:
+        from ingestion import Ingestion
 
 
-if args.public_dataset:
-    from datasets import Neurips2024_public_dataset as public_dataset
-
-    data = public_dataset()
-else:
-    data = Data(input_dir)
-
-
-sys.path.append(program_dir)
-sys.path.append(submission_dir)
-
-from model import Model
+    if not args.codabench:
+        input_dir = args.input
+        output_dir = args.output
+        submission_dir = args.submission
+        program_dir = os.path.join(root_dir_name, "ingestion_program")
+    else:
+        input_dir = "/app/input_data"
+        output_dir = "/app/output"
+        submission_dir = "/app/ingested_program"
+        program_dir = "/app/program"
 
 
-ingestion = Ingestion(data)
+    if args.public_dataset:
+        from datasets import Neurips2024_public_dataset as public_dataset
 
-# Start timer
-ingestion.start_timer()
+        data = public_dataset()
+    else:
+        data = Data(input_dir)
 
-# initialize submission
-ingestion.init_submission(Model)
 
-# fit submission
-ingestion.fit_submission()
-test_settings = {}
-test_settings["systematics"] = {
-    "tes": args.systematics_tes,
-    "jes": args.systematics_jes,
-    "soft_met": args.systematics_soft_met,
-    "w_scale": args.systematics_w_scale,
-    "bkg_scale": args.systematics_bkg_scale,
-}
-test_settings["num_pseudo_experiments"] = args.num_pseudo_experiments
-test_settings["num_of_sets"] = args.num_of_sets
+    sys.path.append(program_dir)
+    sys.path.append(submission_dir)
 
-if args.use_random_mus:
-    test_settings["ground_truth_mus"] = (
-        np.random.uniform(0.1, 3, test_settings["num_of_sets"])
-    ).tolist()
-    test_settings["random_mu"] = True
-    random_settings_file = os.path.join(output_dir, "test_settings.json")
-    with open(random_settings_file, "w") as f:
-        json.dump(test_settings, f)
-else:
-    test_settings["ground_truth_mus"] = data.ground_truth_mus
+    from model import Model
 
-# load test data
-data.load_test_set()
 
-# predict submission
-ingestion.predict_submission(test_settings)
+    ingestion = Ingestion(data)
 
-# compute result
-ingestion.compute_result()
+    # Start timer
+    ingestion.start_timer()
 
-# save result
-ingestion.save_result(output_dir)
+    # initialize submission
+    ingestion.init_submission(Model)
 
-# Stop timer
-ingestion.stop_timer()
+    # fit submission
+    ingestion.fit_submission()
+    test_settings = {}
+    test_settings["systematics"] = {
+        "tes": args.systematics_tes,
+        "jes": args.systematics_jes,
+        "soft_met": args.systematics_soft_met,
+        "w_scale": args.systematics_w_scale,
+        "bkg_scale": args.systematics_bkg_scale,
+    }
+    test_settings["num_pseudo_experiments"] = args.num_pseudo_experiments
+    test_settings["num_of_sets"] = args.num_of_sets
 
-# Show duration
-ingestion.show_duration()
+    if args.use_random_mus:
+        test_settings["ground_truth_mus"] = (
+            np.random.uniform(0.1, 3, test_settings["num_of_sets"])
+        ).tolist()
+        test_settings["random_mu"] = True
+        random_settings_file = os.path.join(output_dir, "test_settings.json")
+        with open(random_settings_file, "w") as f:
+            json.dump(test_settings, f)
+    else:
+        test_settings["ground_truth_mus"] = data.ground_truth_mus
 
-# Save duration
-ingestion.save_duration(output_dir)
+    # load test data
+    data.load_test_set()
+
+    # predict submission
+    ingestion.predict_submission(test_settings)
+
+    # compute result
+    ingestion.compute_result()
+
+    # save result
+    ingestion.save_result(output_dir)
+
+    # Stop timer
+    ingestion.stop_timer()
+
+    # Show duration
+    ingestion.show_duration()
+
+    # Save duration
+    ingestion.save_duration(output_dir)
