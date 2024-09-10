c7fd6fea959bcccc17cc674261789f6fbd511f5e
ragansu <ragansu@gmail.com>
2024-09-10 08:18:17 -0700

    update test script

--- test/Launch_tests.sh
+++ test/Launch_tests.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #SBATCH --account=m4287
 #SBATCH --image=nersc/fair_universe:1298f0a8 
-#SBATCH --qos=debug
+#SBATCH --qos=premium
 #SBATCH -C gpu&hbm80g
 #SBATCH -N 6
 #SBATCH --gpus-per-task=4
-#SBATCH -t 5
+#SBATCH -t 4:00:00
 #SBATCH -J codabench
-#SBATCH --mail-user=ragansu@nersc.gov  # Where to send mail
-#SBATCH --mail-type=ALL        # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --output=codabench_%j.log   # Standard output and error log
 
 now=$(date +"%Y%m%d_%H%M%S")
@@ -23,11 +21,18 @@ echo
 
 working_dir="/global/cfs/cdirs/m4287/hep"
 
+if [ $# -eq 0 ]; then
+  echo "No Model name provided"
+  model_name="simple_one_syst_model"
+else
+  echo "Model name provided: $1"
+  model_name=$1
+fi
 
-srun -n 6 -c 64 shifter bash -c "
+srun -n 2 shifter bash -c "
     List_systematics=(tes jes soft_met bkg_scale ttbar_scale diboson_scale)
     systematics=\${List_systematics[\$SLURM_PROCID]}
     export MODEL_SYST_NAME=\$systematics
-    sh ${working_dir}/HEP-Challenge/test/run_test.sh \$systematics
+    sh ${working_dir}/HEP-Challenge/test/run_test.sh \$systematics ${model_name}
     "
 echo "End"
--- test/run_test.sh
+++ test/run_test.sh
@@ -1,19 +1,27 @@
-echo "Running $@"
+# Description: Run the test for a model with one systematic
+systematics=$1
+model_name=$2
+
+echo Running test for model ${model_name} with systematic ${systematics}
 
 working_dir="/global/cfs/cdirs/m4287/hep"
-
 input_dir="/global/cfs/projectdirs/m4287/data"
 reference_dir="${working_dir}/public_data_neurips/reference_data"
 ingestion_program_dir=${working_dir}/HEP-Challenge/ingestion_program/run_ingestion.py 
 scoring_program_dir=${working_dir}/HEP-Challenge/scoring_program/run_scoring.py
-model_dir=${working_dir}/HEP-Challenge/simple_one_syst_model
 export MAX_WORKERS=30
 
-systematics=$1
-export MODEL_SYST_NAME=\$systematics
+
+if [ "$model_name" = "simple_one_syst_model" ]; then
+    export MODEL_SYST_NAME=$systematics
+fi
+
+
+
+model_dir=${working_dir}/HEP-Challenge/${model_name}
 
 systematics_flag=$(echo $systematics | tr '_' '-')
-output_dir=${working_dir}/HEP-Challenge/test/temp/simple_one_syst_model_${systematics}
+output_dir=${working_dir}/HEP-Challenge/test/temp/${model_name}_${systematics}
 
 mkdir -p ${output_dir}
 mkdir -p ${output_dir}/submission
