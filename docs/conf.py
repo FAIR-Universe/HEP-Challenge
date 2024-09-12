5c5cb660a65cafd4c6fb688c2c0dff1c27d72f08
ragansu <ragansu@gmail.com>
2024-09-12 14:44:35 +0200

    logo fix

--- docs/conf.py
+++ docs/conf.py
@@ -43,3 +43,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "_static/logo.png"
+html_favicon = '_static/logo.png'
