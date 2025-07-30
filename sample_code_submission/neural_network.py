6129331227ed39082c52611e37778d86bb07d003
CHAPUIS guilhem <chapuis.gu@orange.fr>
2025-07-30 10:55:05 +0200

    fit instead of train

--- sample_code_submission/neural_network.py
+++ sample_code_submission/neural_network.py
@@ -42,7 +42,7 @@ class NeuralNetwork:
 
         self.scaler.fit_transform(train_data)
         X_train = self.scaler.transform(train_data)
-        self.model.train(
+        self.model.fit(
             X_train, y_train, sample_weight=weights_train, epochs=100, batch_size=1024, verbose=2
         )
 
